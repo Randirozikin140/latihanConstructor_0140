3f55e1e40cbfdddcbbd3631927a5129000d0565c
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-05-20 19:01:01 +0700

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,35 @@
+//Buatlah programc++  dengan class barang yang memiliki atribut: nama barang,
+//kode barang
+//program memiliki construktor berparameter untuk mengisi
+//nilai nama barang dan kode barang
+//serta memiliki fungsi untuk menampilkan informasi barang
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Barang
+{
+private:
+    string nama_barang;
+    string kode_barang;
+public:
+    Barang(string nama, string kode)
+    {
+        nama_barang = nama;
+        kode_barang = kode;
+    }
+
+    void cetak()
+    {
+        cout << "nama barang: " << nama_barang << endl;
+        cout << "kode barang: " << kode_barang << endl;
+    }
+};
+
+int main()
+{
+    Barang b("laptop", "IRX15");
+    b.cetak();
+    return 0;
+
+}
